81125fc54a866524c5f0e4aebdb1b34ac6913dd1
NH <omnia.rezeka@yahoo.com>
2019-01-19 15:18:55 +0200

    changes

--- app/src/main/java/com/example/nh/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/example/nh/recyclerviewexample/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         contactsData.add(c5);
 
         contactsRv = findViewById(R.id.rv_contacts);
-        //contactsRv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        contactsRv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
         contactsRv.setLayoutManager(new GridLayoutManager(this,2));
         adapter = new ContactAdapter(this,contactsData);
         contactsRv.setAdapter(adapter);
